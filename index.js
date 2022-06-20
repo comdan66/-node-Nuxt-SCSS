08aa50426ac61542b763b38738c8ba961b89ca68
OA Wu <oa.wu@idrip.coffee>
2022-06-20 17:54:02 +0800

    Fix author Fix copyright

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /**
- * @author      OA Wu <comdan66@gmail.com>
+ * @author      OA Wu <oawu.tw@gmail.com>
  * @copyright   Copyright (c) 2015 - 2022, @oawu/nuxt-scss
  * @license     http://opensource.org/licenses/MIT  MIT License
  * @link        https://www.ioa.tw/
